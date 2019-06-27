27b726df9687ccd1e049f8ca8ba54ad71fc3c47a
patrykbaklaj@gmail.com <patrykbaklaj@gmail.com>
2019-06-27 22:45:46 +0100

    Implement router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { Router, Route } from 'react-router-dom';
+import history from '../history';
 import QuestionAdd from './admin/QuestionAdd';
 import QuestionList from './admin/QuestionList';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello from App</h1>
-                <QuestionList />
-                <QuestionAdd />
-            </div>
+            <Router history={history}>
+                <div>
+                    <h1>Hello from App</h1>
+
+                    <Route
+                        path='/admin/questions/list'
+                        component={QuestionList}
+                    />
+                    <Route
+                        path='/admin/questions/add'
+                        component={QuestionAdd}
+                    />
+                </div>
+            </Router>
         );
     }
 }
